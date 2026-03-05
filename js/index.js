0f4f8a8617c5ba0b88ab2c82742e116138f6e911
Nikita Dyukov <dyukovnick@gmail.com>
2026-03-05 19:58:16 +0300

    refactor: move button from index.js to ui.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-// Main JavaScript imports
-import "/js/themes.js";
-import "/js/languages.js";
-
-// ====== Home button ======
-const homeBtn = document.querySelector(".home-btn");
-
-if (homeBtn) {
-  homeBtn.addEventListener("click", () => {
-    window.location.href = "/index.html";
-  });
-}
+// ====== Main JavaScript imports ======
+import "/js/ui.js";
+import "/js/translations.js";
